46371c35a57bac940f8fceb80f7b535462e38ae6
Henri Kähkönen <henri.kahkonen@gmail.com>
2026-09-01 13:05:57 +0300

    Created first version of SNs page

---
+++ src/api/useSnSData.ts
@@ -0,0 +1,47 @@
+import api_base_url from "../config";
+import { useApiResource } from "./useApiResource";
+import type { Language } from "../assets/LanguageContext";
+
+export interface SnsPackDescription {
+    language: Language;
+    description: string;
+}
+
+export interface SnSPack {
+    title: string;
+    slug: string;
+    imgUrl: string;
+    e_url: string;
+    tags: { name: string }[];
+    release_date: string;
+    updated_date: string;
+    likes: Number;
+    translations: SnsPackDescription[];
+    file_list: string[];
+}
+
+export interface SnSChangelogTranslation {
+    language: Language;
+    title: string;
+    body_markdown: string;
+}
+
+export interface SnSChangelogEntry {
+    date: string;
+    title: string;
+    translations: SnSChangelogTranslation[];
+}
+
+export interface SnsDataResponse {
+    packs: SnSPack[];
+    sns_cl: SnSChangelogEntry[];
+}
+
+export function useSnsData() {
+  return useApiResource<SnsDataResponse>("sns-data", () =>
+    fetch(`${api_base_url}/api/get-sns-data/`).then((res) => {
+      if (!res.ok) throw new Error(`Request failed: ${res.status}`);
+      return res.json();
+    })
+  );
+}
